9e8a7baa093932977d774c6944e9bf8fad80a8d9
Dharun Raj D <dharunraj1982002@gmail.com>
2023-01-23 23:49:22 +0530

    third commit

--- src/Homepagefire.js
+++ src/Homepagefire.js
@@ -2,16 +2,15 @@ import { useState } from "react";
 import {storage} from "./firebase-config"
 import { ref,uploadBytes,getDownloadURL} from "firebase/storage";
 import {createWorker} from 'tesseract.js';
-
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import './Homepagefire.css';
 
 
 function Homepagefire() {
     const[imageUpload,setImageUpload]=useState(null);
     const [imageUrl, setImageUrl] = useState(null);
-    const [stringValue, setStringValue] = useState('');
+    const [stringValue, setStringValue] = useState(' ');
     
-
-
     var imageRef="";
     const uploadImage=()=>{
         if(imageUpload==null) 
@@ -22,13 +21,13 @@ function Homepagefire() {
             getDownloadURL(snapshot.ref).then((url) => {
                 setImageUrl(url);
                 
+                
               });
         });    
     };
 
-
     const imageToText=async()=>{
-
+        setStringValue('');
         const worker = createWorker({
             logger: m => console.log(m),
           });
@@ -38,30 +37,40 @@ function Homepagefire() {
             await worker.loadLanguage('eng');
             await worker.initialize('eng');
             const { data: { text } } = await worker.recognize(imageUrl);
-            setStringValue(text);
-          
-
-          
-    
+            setStringValue(text);    
     }
-
-
-
 
     const onFileChange = (event) => {
         setImageUpload(event.target.files[0]);
       };
 
     return(
-        <div className="App">
-            <input type="file" onChange={onFileChange} />
-            <button onClick={uploadImage}>Upload Image</button> 
-            {imageUrl&&<img src={imageUrl} alt="uploaded image"/>}
-            <button onClick={imageToText}>Generate text</button>
-            <h5>{stringValue}</h5>
+        <body>
+    <section>
+        
+                <div className="message">
+                    <input type="file" onChange={onFileChange} />
+                    <FileUploadOutlinedIcon className="svg_home" onClick={uploadImage}/> 
+                    {imageUrl&&<img src={imageUrl} alt="" height="50%" width="60%"/>}
+                
+                <br/>
+                <button onClick={imageToText}>Generate text</button>
+                </div>
+                </section>
+                <div className="textbox">
+                {
+                    stringValue==="" ? 
+                        <div className="loader align-self-center" ></div>
+                        :
+                        <div classname="textbox">
+                            <h2>{stringValue}</h2>
+                        </div>
+                }
+            </div>              
+            </body>
             
-                      
-        </div>
+        
+  
     )   
 }
 
