c6d4c5d7a9f8737eb8a72f8d687d4ec53a73a705
Dharun Raj D <dharunraj1982002@gmail.com>
2023-01-25 19:53:36 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Homepagefire/>}/>
+        <Route path="/" element={<Homepagefire/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>}/>
       
